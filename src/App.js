16092d91c366158f2cb47b408bdb60eb0f687423
MaximKotovich <makckotok2020@gmail.com>
2021-08-11 13:27:27 +0300

    update11.08

--- src/App.js
+++ src/App.js
@@ -8,57 +8,61 @@ import Modal from "./modalWindow/modal"
 
 function App() {
 
-  const img = [
-    <img src={img1} />,
-    <img src={img2} />,
-    <img src={img3} />,
-    <img src={img4} />,
-    <img src={img4} />,
-    <img src={img4} />,
-  ]
-
-
-
-
-
   const photoCard = [
     {
       id: 1,
+      imgUrl: <img src={img1} />,
       title: "Card 1",
       description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa sint nostrum harum omnis delectus eius optio corporis reiciendis atque ab voluptatum consectetur, enim, quod explicabo inventore sapiente, laborum facere quis Magnam similique vel quis soluta nisi voluptas iure nesciunt adipisci nihil eius perspiciatis, consequuntur alias magni consectetur eaque illum, odio earum. Est nam temporibus ducimus veniam tempore possimus quibusdam molestias."
     },
     {
       id: 2,
+      imgUrl: <img src={img2} />,
       title: "Card 2",
       description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa sint nostrum harum omnis delectus eius optio corporis reiciendis atque ab voluptatum consectetur, enim, quod explicabo inventore sapiente, laborum facere quis Magnam similique vel quis soluta nisi voluptas iure nesciunt adipisci nihil eius perspiciatis, consequuntur alias magni consectetur eaque illum, odio earum. Est nam temporibus ducimus veniam tempore possimus quibusdam molestias."
     },
     {
       id: 3,
+      imgUrl: <img src={img3} />,
       title: "Card 3",
       description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa sint nostrum harum omnis delectus eius optio corporis reiciendis atque ab voluptatum consectetur, enim, quod explicabo inventore sapiente, laborum facere quis Magnam similique vel quis soluta nisi voluptas iure nesciunt adipisci nihil eius perspiciatis, consequuntur alias magni consectetur eaque illum, odio earum. Est nam temporibus ducimus veniam tempore possimus quibusdam molestias."
     },
     {
       id: 4,
+      imgUrl: <img src={img4} />,
       title: "Card 4",
       description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa sint nostrum harum omnis delectus eius optio corporis reiciendis atque ab voluptatum consectetur, enim, quod explicabo inventore sapiente, laborum facere quis Magnam similique vel quis soluta nisi voluptas iure nesciunt adipisci nihil eius perspiciatis, consequuntur alias magni consectetur eaque illum, odio earum. Est nam temporibus ducimus veniam tempore possimus quibusdam molestias."
     },
     {
       id: 5,
-      title: "Card 4",
+      imgUrl: <img src={img4} />,
+      title: "Card 5",
       description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa sint nostrum harum omnis delectus eius optio corporis reiciendis atque ab voluptatum consectetur, enim, quod explicabo inventore sapiente, laborum facere quis Magnam similique vel quis soluta nisi voluptas iure nesciunt adipisci nihil eius perspiciatis, consequuntur alias magni consectetur eaque illum, odio earum. Est nam temporibus ducimus veniam tempore possimus quibusdam molestias."
     },
     {
       id: 6,
-      title: "Card 4",
+      imgUrl: <img src={img4} />,
+      title: "Card 6",
       description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa sint nostrum harum omnis delectus eius optio corporis reiciendis atque ab voluptatum consectetur, enim, quod explicabo inventore sapiente, laborum facere quis Magnam similique vel quis soluta nisi voluptas iure nesciunt adipisci nihil eius perspiciatis, consequuntur alias magni consectetur eaque illum, odio earum. Est nam temporibus ducimus veniam tempore possimus quibusdam molestias."
-    },
+    }
   ]
+ 
+const cardFunc = (item) => {
+   setCurrentCard(item)
+  }
+
+
   const [modalActive, setModalActive] = useState(false)
+  const [currentCard, setCurrentCard] = useState()
+
+
+
   const visibleCard = photoCard.map((item) => {
+  
     return (
       <div className="card" key={item.id}>
-        <div className="card-image" onClick={() => { setModalActive(true) }}>
-          {img[item.id - 1]}
+        <div className="card-image" onClick={() => {setModalActive(true); cardFunc(item)}}>
+          {item.imgUrl}
         </div>
         <div className="card-title">
           {item.title}
@@ -69,35 +73,21 @@ function App() {
       </div>
     )
   })
+
   return (
-    <div>
-      {/* <Modal 
+    <div>      
+      <Modal 
         active={modalActive} 
         setActive={setModalActive} 
-        id={photoCard.id}
-        title={photoCard.title}
-        description={photoCard.description}
-        />  */}
-      <div className={modalActive ? "modalWindow active" : "modalWindow"} onClick={() => setModalActive(false)}>
-        <div className="obratnuj-zvonok" autocomplete="off" onClick={e => e.stopPropagation()}>
-          <div className="card" key={photoCard.id}>
-            <div className="card-image">
-              {/* {img[item.id-1]} */}
-            </div>
-            <div className="card-title">
-              {photoCard.title}
-            </div>
-            <div className="card-description">
-              {photoCard.description}
-            </div>
-          </div>
-        </div>
-      </div>
+        photoCard={photoCard}
+        card={currentCard} 
+      />  
       <div className="container">
         {visibleCard}
       </div>
     </div>
   );
+ 
 }
 
 export default App;
--- src/modalWindow/modal.js
+++ src/modalWindow/modal.js
@@ -2,22 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import "./modal.css";
 
-export default function Modal({active, setActive}, props) {
-    return( 
+export default function Modal({active, setActive, card}) {
+  if (card){
+  return( 
     <div className={active ? "modalWindow active" : "modalWindow"} onClick={() => {setActive(false)}}>       
-        <div className="obratnuj-zvonok" autocomplete="off" onClick = {e => e.stopPropagation()}>
-        <div className="card" key = {props.id}>       
-          <div className="card-image">
-              {/* {img[item.id-1]} */}
+        <div className="obratnuj-zvonok" autocomplete="off">
+        <div className="card-item" >       
+          <div className="image">
+           {card.imgUrl}            
           </div>
-          <div className="card-title">
-            {props.title}
+          <div className="title">
+            {card.title}
           </div>
-          <div className="card-description">
-            {props.description} 
+          <div className="description">
+            {card.description}
           </div>
-          </div>
+          </div>       
         </div> 
     </div>   
     )
+  } else { return(null) }
 }
